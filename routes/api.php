48771c408e61d81d56e98670e16dd5ecc6d130f6
Linar Galiakhmetov <linar.nk@gmail.com>
2023-02-02 13:48:18 +0300

    Add Seeder for Users and config route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::prefix('user')->group(function () {
+        Route::get("/", function () {
+            return Auth::user();
+        })->name('user');
+    });
 });
